92650d5053f1bdccd9b30e1498468899d9fdeba8
bmanjuu <bettyofung@gmail.com>
2016-06-16 09:28:33 -0400

    passed all tests for FISCard

--- OOP-Cards-Model/FISAppDelegate.m
+++ OOP-Cards-Model/FISAppDelegate.m
@@ -1,11 +1,16 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISCard.h"
 
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FISCard *defaultCard = [[FISCard alloc] init];
+    NSLog(@"%@", [defaultCard description]);
+    
     return YES;
 }
 
---
+++ OOP-Cards-Model/FISCardDeck.m
@@ -0,0 +1,36 @@
+//
+//  FISCardDeck.m
+//  OOP-Cards-Model
+//
+//  Created by Betty Fung on 6/16/16.
+//  Copyright © 2016 Al Tyus. All rights reserved.
+//
+
+#import "FISCardDeck.h"
+
+@implementation FISCardDeck
+
+-(FISCard *) drawNextCard{
+    return nil;
+}
+
+
+
+-(void)resetDeck{
+    
+}
+
+
+
+-(void)gatherDealtCards{
+    
+}
+
+
+
+-(void)shuffleRemainingCards{
+
+}
+
+
+@end
